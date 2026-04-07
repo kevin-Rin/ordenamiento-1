6e5c6065e8665dc9467725d1c87860449c6194a4
Kevin Rincon <est.kevins.rincon@unimilitar.edu.co>
2026-04-07 09:36:45 -0500

    Solución inicial ordenamien to por burbuja (contiene erroess que deben corregirse)

--- ordenamiento/src/main/java/Ordenamiento.java
+++ ordenamiento/src/main/java/Ordenamiento.java
@@ -6,7 +6,7 @@ public class Ordenamiento {
     }
     private static void ordenar(int v[]) {
         for (int i = 0; i < v.length-1; i++) {
-            for (int j = i+1; j < v.length -1; j++) {
+            for (int j = i+1; j < v.length ; j++) {
                 if (v[i] > v[j]) {
                     int w = v[j];
                     v[j] = v[i];
