8be70f9cea9a1e35d8d38f9bf9f81ffefe5e504c
Kevin Rincon <est.kevins.rincon@unimilitar.edu.co>
2026-04-07 10:52:26 -0500

    implemente otro metodo de ordenamiento llamado buble

--- ordenamiento/src/main/java/Ordenamiento.java
+++ ordenamiento/src/main/java/Ordenamiento.java
@@ -15,17 +15,19 @@ public class Ordenamiento {
         arr[i]= rand.nextInt(100);
         
         }
-         ordenar(arr);
+         burbuja(arr);
         System.out.println(java.util.Arrays.toString(arr));
         
     }
-    private static void ordenar(int v[]) {
-        for (int i = 0; i < v.length-1; i++) {
-            for (int j = i+1; j < v.length ; j++) {
-                if (v[i] > v[j]) {
-                    int w = v[j];
-                    v[j] = v[i];
-                    v[i] = w;
+    private static void burbuja(int v[]) {
+        int n = v.length;
+        
+        for (int i = 0; i < n -1; i++) {
+            for (int j = 0; j < n -1 -i; j++) {
+                if (v[j] > v[j+1]) {
+                    int aux = v[j];
+                    v[j] = v[j+1];
+                    v[j+1] = aux;
                 }
             }
         }
