534ff96e7c55ad3a466cbbcdb327a837d6bc69cc
Kevin Rincon <est.kevins.rincon@unimilitar.edu.co>
2026-04-07 10:29:38 -0500

    se implemento metodo para que se generen numeros aleatorios

--- ordenamiento/src/main/java/Ordenamiento.java
+++ ordenamiento/src/main/java/Ordenamiento.java
@@ -7,12 +7,13 @@ public class Ordenamiento {
         System.out.println("ingrese el tamaño del arreglo");
         
         Scanner teclado = new Scanner(System.in);
-        
+        Random rand = new Random();
         int n = teclado.nextInt();  
         int arr[] = new int [n];
-        for (int i = 0; i < n; i++){
-            System.out.println("ingrese numero para la pocición "+ i +":");  
-        arr[i]= teclado.nextInt();
+        
+        for (int i = 0; i < n; i++){  
+        arr[i]= rand.nextInt(100);
+        
         }
          ordenar(arr);
         System.out.println(java.util.Arrays.toString(arr));
